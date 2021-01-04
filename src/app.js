90039de554ae1843091a0550f5a4b40f5191eb98
Lara Samuelson <lara.samuelson@gmail.com>
2021-01-04 15:45:16 -0800

    remove token

--- src/app.js
+++ src/app.js
@@ -11,27 +11,11 @@ const signInRouter = require('./signIn/signIn-router')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common'
-
   app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
     skip: () => NODE_ENV === 'test',
   }))
   app.use(cors())
   app.use(helmet())
-
-// app.use(function validateBearerToken(req, res, next) {
-//     const apiToken = process.env.API_TOKEN
-//     const authToken = req.get('Authorization')
-   
-//     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//       logger.error(`Unauthorized request to path: ${req.path}`)
-//       return res.status(401).json({ error: 'Unauthorized request' })
-//     }
-    
-//     next()
-// })
 
 
 app.use('/api/auth', signUpRouter)
